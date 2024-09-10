3ff292fcf7a502c722d66a9237258fc8bb1af633
Garth Carey <Garthcarey0862@gmail.com>
2024-09-10 15:09:05 -0600

    complete Week 2 demo

--- app/page.js
+++ app/page.js
@@ -1,10 +1,21 @@
+import Headings from "./week-2/headings"
+import Link from "next/link"
+
+
 export default function Page() {
   return (
     <main>
-      <div>
-        <h1>My world</h1>
-        <p>Hello world </p>
-      </div>
+      <h1>Web Dev 2 Baby </h1>
+        <Headings/>
+        <ul>
+          <li>
+            <Link href="week-2">
+              Week 2
+            </Link>
+          </li>
+
+        </ul>
+        
     </main>
   )  
 }
--- app/week-2/page.js
+++ app/week-2/page.js
@@ -1,13 +1,17 @@
 import Headings from "./headings.js";
 
 export default function Page() {
+    let a = 10;
+    let b = 25;
+    let c = a + b;
   return (
+    // <></> is a fragment, a way to group multiple elements in a single return statement
+   // {} is used to switch between JSX and JavaScript
     <main>
         <Headings/>
-        <Headings/>
-        <Headings/>
-        <Headings/>
+ 
+        <p>Sum of {a} and {b} is {c} </p>
 
     </main>
-  )  
+  );  
 }
