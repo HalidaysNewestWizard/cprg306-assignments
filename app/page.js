8a7c8ca22a7f0ccd460f5073054e537eb8f5a62d
Garth Carey <Garthcarey0862@gmail.com>
2024-11-05 12:01:22 -0700

    picture changes

--- app/page.js
+++ app/page.js
@@ -1,8 +1,8 @@
-import Headings from "./week-2/headings";
-import Link from "next/link";
-import Profile from "./week-3/profile";
-import pictureJson from "./picture.json";
-import Image from "next/image";
+import Headings from './week-2/headings';
+import Link from 'next/link';
+import Profile from './week-3/profile';
+import pictureJson from './picture.json';
+import Image from 'next/image';
 
 export default function Page() {
   return (
@@ -12,33 +12,35 @@ export default function Page() {
 
       <ul>
         <li>
-          <Link href="week-2">Week 2 Assignment</Link>
+          <Link href='week-2'>Week 2 Assignment</Link>
         </li>
         <li>
-          <Link href="week-3">Week 3 Assignment</Link>
+          <Link href='week-3'>Week 3 Assignment</Link>
         </li>
         <li>
-          <Link href="week-4">Week 4 Assignment</Link>
+          <Link href='week-4'>Week 4 Assignment</Link>
         </li>
         <li>
-          <Link href="week-5">Week 5 Assignment</Link>
+          <Link href='week-5'>Week 5 Assignment</Link>
         </li>
         <li>
-          <Link href="week-6">Week 6 Assignment</Link>
+          <Link href='week-6'>Week 6 Assignment</Link>
         </li>
         <li>
-          <Link href="week-7">Week 7 Assignment</Link>
+          <Link href='week-7'>Week 7 Assignment</Link>
         </li>
         <li>
-          <Link href="week-8">Week 8 Assignment</Link>
+          <Link href='week-8'>Week 8 Assignment</Link>
         </li>
       </ul>
       <ul>
         {pictureJson.map((picture) => (
-          <li key={picture.name} className=" my-5 w-44 h-60 rounded-md">
+          <li
+            key={picture.name}
+            className=' my-5 w-44 h-60 rounded-md'>
             <p>
-              <Image
-                className="w-44 h-88 rounded-md"
+              <img
+                className='w-44 h-88 rounded-md'
                 src={picture.imageUrl}
                 alt={picture.name}
                 width={240}
